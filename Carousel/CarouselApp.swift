43abf031177a89ea6572ae2286b9ee5ff712778f
Jundan Harish <gedang.chocochips@gmail.com>
2022-12-10 10:34:42 +0700

    Initial Commit

---
+++ Carousel/CarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselApp.swift
+//  Carousel
+//
+//  Created by Jundan Harish on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
